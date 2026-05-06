63757002a7f4f4f55d0c21586fe5ffff87c6a8bb
kikehil <kikehil@example.com>
2026-05-06 09:47:40 -0600

    Config: cambiar puerto a 3010 para evitar conflicto en VPS

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = 3010;
 
 // Configuración de la conexión a PostgreSQL.
 const pool = new Pool({
